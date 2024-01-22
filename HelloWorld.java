88041aec25c4306c8b840ea111cc12b4daad6565
Ankit-Kum <61354499+Ankit-Kum@users.noreply.github.com>
2024-01-22 09:02:42 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("-----------------------");
+        System.out.println("--------------------");
 
 	System.out.println("Hello Guys");
 	
-	System.out.println("Hello, UP");
+	System.out.println("Hello, bye");
         
-	System.out.println("-------------------------");
+	System.out.println("----------------------");
     }
 }
 
